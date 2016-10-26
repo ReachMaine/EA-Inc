b84ccd1f24b94a838b02b93bece9634980d0721b
Linda Speight <linda@reachmaine.com>
2016-10-26 10:30:11 -0400

    Add woocommerce customizations
    - Hide SKU

---
+++ ea-inc/custom/woocommerce.php
@@ -0,0 +1,4 @@
+<?php
+
+// to remove sku from everywhere....
+add_filter( 'wc_product_sku_enabled', '__return_false' );
--- ea-inc/functions.php
+++ ea-inc/functions.php
@@ -1,12 +1,14 @@
 <?php
+	require_once(get_stylesheet_directory().'/custom/woocommerce.php');
+	
 	$cwk_thumbimg = array(200, 999); // size of featured image in archive/category blog
 	$cwk_postimg = array(200, 999); // size of featured image on single post.
 	add_image_size( 'cwk-slider', 1420, 447, true ); // Slider
-	
+
 	add_action('after_setup_theme', ea_setup);
 	/**  ea_setup
 	*  init stuff that we have to init after the main theme is setup.
-	* 
+	*
 	*/
 	function ea_setup() {
 	 /* do stuff ehre. */
@@ -14,12 +16,12 @@
 		add_action('login_head', 'add_favicon');
 		add_action('admin_head', 'add_favicon');
 	}
-	
+
 	function add_favicon() {
 	  	$favicon_url = get_stylesheet_directory_uri() . '/images/admin-favicon.ico';
 		echo '<link rel="shortcut icon" href="' . $favicon_url . '" />';
 	}
-	
+
 	/* recommended from woothemes  -  new server with old mod_security settings */
 	add_action( 'wp_enqueue_scripts', 'custom_frontend_scripts' );
 	function custom_frontend_scripts() {
