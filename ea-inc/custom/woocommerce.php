b61c6db8e55aa7ed4ceeb19b344bb0608d78a29a
Linda Speight <linda@reachmaine.com>
2016-10-26 11:48:56 -0400

    single product stuff
    - remove title to description
    - remove navigation from single product.

--- ea-inc/custom/woocommerce.php
+++ ea-inc/custom/woocommerce.php
@@ -2,3 +2,8 @@
 
 // to remove sku from everywhere....
 add_filter( 'wc_product_sku_enabled', '__return_false' );
+
+add_filter ('woocommerce_product_description_tab_title','eainc_remove_descr_tab_title' );
+function eainc_remove_descr_tab_title($title) {
+    return '';
+}
